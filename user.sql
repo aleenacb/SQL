57398d24e038dabc0ecb1859c7b9e9c84d1e9af5
aleenacb <aleenacbenoy@gmail.com>
2026-02-14 12:41:03 +0530

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,29 @@
+/*DROP TABLE International_user;
+
+CREATE TABLE International_user(
+    UserId INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    lastName CHAR(50)
+);*/
+
+/*Date and DateTime
+CREATE TABLE Orders(
+OrderId INT PRIMARY KEY,
+OrderDate DATE,
+OrderTime TIME,
+ShippedAT DATETIME
+);*/
+
+/*Binary datatype in SQL
+CREATE TABLE Production_images(
+ImageId INT PRIMARY KEY,
+ImageName VARCHAR(100)
+);*/
+
+/*Boolean datatype*/
+CREATE TABLE form(
+formId INT PRIMARY KEY,
+IsActive INTEGER,
+IsVerified INTEGER
+);
+
