9ed557253e1610edaf5bc27ddc4b8c19cd6eb6b9
aleenacb <aleenacbenoy@gmail.com>
2026-02-26 22:48:39 +0530

    Add files via upload

---
+++ TemporaryTable.sql
@@ -0,0 +1,11 @@
+create database EmpDetails;
+use EmpDetails;
+
+create table EmpDetails(
+Empid int, 
+Empname varchar(25)
+);
+
+insert into EmpDetails values(1, "James"),(02, "MIke");
+
+select * from EmpDetails;
